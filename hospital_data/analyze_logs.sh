c939784d7fe4b9bfc7a403d74c8dd388b4a5f2b0
Oluwatomi-Thompson <o.thompson@alustudent.com>
2025-06-03 03:54:33 -0700

    Made changes to analyze_logs file

--- hospital_data/analyze_logs.sh
+++ hospital_data/analyze_logs.sh
@@ -1,2 +1,28 @@
 #!/bin/bash
 
+echo "Which log do you want to archive?"
+echo "1) Heart Rate"
+echo "2) Temperature"
+echo "3) Water Usage"
+read -p "Enter 1, 2, or 3: " choice
+
+case "$choice" in
+  1)
+    LOG_FILE="heart_rate.log"
+    ARCHIVE_SUBDIR="heart_data_archive"
+    ;;
+  2)
+    LOG_FILE="temperature.log"
+    ARCHIVE_SUBDIR="temperature_data_archive"
+    ;;
+  3)
+    LOG_FILE="water_usage.log"
+    ARCHIVE_SUBDIR="water_usage_data_archive"
+    ;;
+  *)
+    echo "Invalid choice."
+    exit 1
+    ;;
+esac
+
+
