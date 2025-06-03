0ab18571bba2e2675b9c999cbbf8897de0550158
Oluwatomi-Thompson <o.thompson@alustudent.com>
2025-06-03 15:15:24 -0700

    Fixed error in file

--- archive_logs.sh
+++ archive_logs.sh
@@ -1,54 +1,58 @@
 #!/bin/bash
 
 
+
 echo "Select log to archive:"
 echo "1) Heart Rate (heart_rate.log)"
 echo "2) Temperature (temperature.log)"
 echo "3) Water Usage (water_usage.log)"
 read -p "Enter choice (1-3): " choice
 
-if [ "$choice" = "1" ];then
-	LOG_FILE="heart_rate.log"
-	ARCHIVE_SUBDIR="heart_data_archive"
-elif [ "$choice" = "2" ];then
-	 LOG_FILE="temperature.log"
-         ARCHIVE_SUBDIR="temperature_data_archive"
- elif [ "$choice" = "3" ];then
-	 LOG_FILE="water_usage.log"
-         ARCHIVE_SUBDIR="water_data_archive"
- else
-	 echo "Invalid Choice, Please choose either 1,2, or 3"
-	 exit 1
-fi
 
-
-LOG_DIR="hospital_data/active_logs"
-ARCHIVE_DIR="hospital_data/archived_logs"
-TIMESTAMP=$(date "+%Y-%m-%d_%H-%M-%S")
-
-
-if [ ! -f "$LOG_DIR/$LOG_FILE" ]; then
-    echo "Error: Log file '$LOG_FILE' not found."
+if [ "$choice" = "1" ]; then
+    log_file="heart_rate.log"
+    archive_subdir="heart_data_archive"
+elif [ "$choice" = "2" ]; then
+    log_file="temperature.log"
+    archive_subdir="temperature_data_archive"
+elif [ "$choice" = "3" ]; then
+    log_file="water_usage.log"
+    archive_subdir="water_data_archive"
+else
+    echo "Invalid Choice, Please choose either 1, 2, or 3"
     exit 1
 fi
 
-if [ ! -d "$ARCHIVE_DIR/$ARCHIVE_SUBDIR" ]; then
-    echo "Error: Archive folder '$ARCHIVE_DIR' not found."
-    mkdir -p "$ARCHIVE_DIR/$ARCHIVE_SUBDIR/"
+
+
+log_dir="hospital_data/active_logs"
+archive_dir="hospital_data/archived_logs"
+
+
+if [ ! -f "$log_dir/$log_file" ]; then
+    echo "Error: Log file '$log_file' not found."
+    exit 1
 fi
 
 
+if [ ! -d "$archive_dir/$archive_subdir" ]; then
+    mkdir -p "$archive_dir/$archive_subdir"
+fi
 
-
-if mv "$LOG_DIR/$LOG_FILE" "$ARCHIVE_DIR/$ARCHIVE_SUBDIR/${LOG_FILE%.log}_$TIMESTAMP.log";then
-	echo "Successfully archived '$LOG_FILE' to '$ARCHIVE_DIR/$ARCHIVE_SUBDIR/${LOG_FILE%.log}_$TIMESTAMP.log'"
+timestamp=$(date "+%Y-%m-%d_%H-%M-%S")
+new_name="${log_file%.log}_$timestamp.log"
+if mv "$log_dir/$log_file" "$archive_dir/$archive_subdir/$new_name"; then
+    echo "Successfully archived '$log_file' to '$archive_dir/$archive_subdir/$new_name'"
 else
-	echo "Failed to archive $log_file"
-	exit 1
+    echo "Failed to archive $log_file"
+    exit 1
 fi
-if touch "$LOG_DIR/$LOG_FILE";then
-	echo "Created new empty log file: $LOG_FILE"
+
+
+if touch "$log_dir/$log_file"; then
+    echo "Created new empty log file: $log_file"
 else
-	echo "Failed to create new log file"
-	exit 1
+    echo "Failed to create new log file"
+    exit 1
 fi
+
