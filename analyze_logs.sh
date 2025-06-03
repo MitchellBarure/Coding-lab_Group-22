65b6887f9a152c811ba89c656ee57c00961ec0d7
aackun <sowhatmanuelle23@gmail.com>
2025-06-03 05:17:55 -0700

    analysis script

--- analyze_logs.sh
+++ analyze_logs.sh
@@ -1,9 +1,10 @@
+
 #!/bin/bash
 
 #creating variables for file path
 
-log_dir="/hospital_data/active_logs"
-report_file="/hospital_data/reports/analysis_report.txt"
+log_dir="/Coding-lab_Group-22/hospital_data/active_logs"
+report_file="/Coding-lab_Group-22/hospital_data/reports/analysis_report.txt"
 
 #asking for user's choice
 
@@ -14,3 +15,21 @@ echo "3) Water usage (water_usage.log)"
 read -p "Select your choice (1-3): " choice
 
 #validating user input
+
+case $choice  in 
+  1) file="heart_rate_log.log" ;;
+  2) file="temperature_log.log" ;;
+  3) file="water_usage_log.log" ;;
+  *) echo "Invalid Input."; exit 1 ;;
+esac
+
+#check if the file exists
+
+file_path="$log_dir/$file"
+
+if [[ -f "$file_path" ]]; then
+ echo "Log file exists: $file_path"
+else
+ echo "Log file does not exist: $file_path"
+ exit 1
+fi
